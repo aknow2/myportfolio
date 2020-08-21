98c2840ba02cc9bee737b9a964b3d39153fefccd
金山智昭 <kanayamatomoaki@kanayamatomoakinoiMac-2.local>
2020-08-21 23:49:47 +0900

    update

--- folio/src/Background.tsx
+++ folio/src/Background.tsx
@@ -57,6 +57,15 @@ function createIbo(data: number[], gl: WebGLRenderingContext){
 	return ibo;
 }
 
+const rotate = (x: number, y: number, deg: number): [number, number] => {
+	const rad = deg * Math.PI/180;
+	const cos = Math.cos(rad);
+	const sin = Math.sin(rad);
+	const nx = cos * x - sin * y;
+	const ny = sin * x + cos * y;
+	return [nx, ny];
+}
+
 function renderParticle(c: HTMLCanvasElement){
 	c.width = 500;
   c.height = 500;
@@ -97,40 +106,27 @@ function renderParticle(c: HTMLCanvasElement){
 	mat4.translate(projectionMat, projectionMat, [0,0, -1])
 	const modelMat = mat4.create();
 
-	const rad = 30 * Math.PI/180;
-	const y = Math.cos(rad);
-	const x = Math.sin(rad);
-	const y2 = Math.cos(rad*-1);
-	const x2 = Math.sin(rad*-1);
-
-	const rotate = (x: number, y: number, deg: number): [number, number] => {
-		const rad = deg * Math.PI/180;
-		const nx = Math.cos(rad) * x - Math.sin(rad) * y;
-		const ny = Math.sin(rad) * x + Math.cos(rad) * y;
-		return [nx, ny];
-	}
-
-	const [x3, y3] = rotate(x2, y2, 60);
-	const [x4, y4] = rotate(x3, y3, 60);
 	const position = [
 		0.0,  0.0,  0.0,
-		x, y, 0.0,
-		x2, y2, 0.0,
-		x3, y3,  0.0,
-		x4, y4, 0.0,
 	];
 	const color = [
 		1.0, 0.0, 0.0, 1.0,
-		0.0, 1.0, 0.0, 1.0,
-		0.0, 0.0, 1.0, 1.0,
-		0.0, 1.0, 0.0, 1.0,
-		0.0, 1.0, 0.0, 1.0,
 	];
-	const index = [
-		0, 1, 2,
-		0, 2, 3,
-		0, 3, 4,
-	];
+	const index = [];
+	const rad = 30 * Math.PI/180;
+	const y = Math.cos(rad);
+	const x = Math.sin(rad);
+
+	position.push(x,y,0);
+	color.push(1.0, 0.0, 1.0, 1.0)
+	for(let i=0; i<6; i++) {
+		const [px, py ] = position.slice(-3);
+		const [nx, ny] = rotate(px, py, 60);
+		position.push(nx, ny, 0);
+		color.push(0.0, 0.0, 1.0, 1.0)
+		index.push(0, i+1, i+2);
+	}
+
 	const pos = createVbo(position, gl) as WebGLBuffer;
 	const col = createVbo(color, gl) as WebGLBuffer;
 	const ibo = createIbo(index, gl);
